ac869ab248d7e40ea309665fa4dbb689fef126f6
Frank Hancock <fjwhancock@gmail.com>
2018-07-13 19:30:58 -0400

    Start REST API Spring Project

---
+++ src/main/java/bookmarks/RestSpringBeginnerApplication.java
@@ -0,0 +1,12 @@
+package bookmarks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringBeginnerApplication.class, args);
+	}
+}
